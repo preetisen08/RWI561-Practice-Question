eece7d9cdf67fc249b8e7eab3e993ab8d7d5379a
preetisen08 <138500213+preetisen08@users.noreply.github.com>
2024-05-08 11:40:13 +0530

    Update AddAllMethod.java

--- Collection Framework/ArrayyList/AddAllMethod.java
+++ Collection Framework/ArrayyList/AddAllMethod.java
@@ -1,35 +1,36 @@
 package ArrayyList;
 
-import java.util.ArrayDeque;
-
+import java.util.ArrayList;
 
 public class AddAllMethod {
-	public static void main(String args[])throws Exception { 
+	 public static void main(String args[]) 
+	    { 
+	        ArrayList<Integer> arrlist1 = new ArrayList<Integer>(); 
 	  
-		    ArrayDeque<Integer> ArrayDeque1 = new ArrayDeque<Integer>(); 
-		    ArrayDeque1.add(12); 
-		    ArrayDeque1 .add(20); 
-		    ArrayDeque1 .add(45); 
+	        arrlist1.add(12); 
+	        arrlist1.add(20); 
+	        arrlist1.add(45); 
 	  
-	        System.out.println("Printing ArrayDeque1:"); 
-	        for (Integer number : ArrayDeque1 )  
+	        System.out.println("Printing list1:"); 
+	        for (Integer number : arrlist1)  
 	            System.out.println("Number = " + number);         
 	  
-	        ArrayDeque<Integer> ArrayDeque2 = new ArrayDeque<Integer>(5); 
+	        ArrayList<Integer> arrlist2 = new ArrayList<Integer>(); 
 	  
-	        ArrayDeque2.add(25); 
-	        ArrayDeque2.add(30); 
-	        ArrayDeque2.add(31); 
-	        ArrayDeque2.add(35); 
-
-	        System.out.println("Printing ArrayDequelist2:"); 
-	        for (Integer number : ArrayDeque2)  
-	            System.out.println("Number = " + number);        
-	        ArrayDeque2 .addAll(ArrayDeque2); 
+	        arrlist2.add(25); 
+	        arrlist2.add(30); 
+	        arrlist2.add(31); 
+	        arrlist2.add(35); 
+	  
+	        System.out.println("Printing list2:"); 
+	        for (Integer number : arrlist2)  
+	            System.out.println("Number = " + number);         
+	        
+	        arrlist1.addAll(arrlist2); 
 	  
 	        System.out.println("Printing all the elements"); 
-	   
-	        //for (Integer number : ArrayDeque2 )  
-	          //  System.out.println("Number = " + number);         
+	      
+	        for (Integer number : arrlist1)  
+	            System.out.println("Number = " + number);         
 	    } 
 	} 
